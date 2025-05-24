91e57da3a803d2992bd7f03302566a3b20bf7927
DANSKI423319 <danielskillman357@yahoo.co.uk>
2025-05-24 13:29:14 +0100

    Add todo model

---
+++ api/app/Models/Todo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    /** @use HasFactory<\Database\Factories\TodoFactory> */
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'task',
+        'completed_at',
+    ];
+}
